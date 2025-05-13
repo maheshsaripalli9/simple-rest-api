b3e0946d435d2f4b517f7641a59d3d287f5e880b
Mahesh Saripalli <maheshsaripalli9@gmail.com>
2025-05-13 03:24:57 +0000

    Introduce `/unravel` and add timezone support to `/helloworld`
    * Added support for IANA timezone query parameter to /helloworld route
    * Introduced new /unravel route for JSON object flattening

--- README.md
+++ README.md
@@ -11,3 +11,45 @@ This repository contains my solution to the Breakthrough Energy contrails coding
     make run
     ```
 3.  Once the server is running, open your web browser and navigate to `http://127.0.0.1:5000/helloworld` to see the "Hello World!" message.
+
+# API Endpoints
+# # /helloworld
+Returns a "Hello World!" message. Optionally accepts a tz query parameter to return the current time in a specific timezone.
+
+Example Requests:
+
+Get a plain "Hello World!" message:
+```bash
+curl http://127.0.0.1:5000/helloworld
+```
+
+Get a JSON response with the "Hello World!" message:
+```bash
+curl -H "Accept: application/json" http://127.0.0.1:5000/helloworld
+```
+
+Get the current time in a specific timezone (e.g. America/New_York):
+```bash
+curl -X GET -H "Accept: application/json" 'http://127.0.0.1:5000/helloworld?tz=America/New_York'
+```
+
+# # /unravel
+Takes a JSON object as input and returns a flattened list of keys and values.
+
+Example Request
+
+Unravel a JSON object:
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"key1": {"keyA": ["foo", 0, "bar"]}, "some other key": 2, "finally": "end"}' http://127.0.0.1:5000/unravel
+```
+This should return a JSON response with the flattened list of keys and values.
+```bash
+["key1","keyA","foo",0,"bar","some other key",2,"finally","end"]
+```
+
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+import pytz
 
 app = Flask(__name__)
 
 @app.route('/helloworld', methods=['GET'])
 def helloworld():
-    """A simple endpoint that returns "Hello World!"
     """
-    return "Hello World!"
+    Returns a "Hello World!" message. 
+    Optionally accepts a tz query parameter to return the current time in a specific timezone.
+    """
+    message = "Hello World!"
+    tz_param = request.args.get('tz')
+
+    if tz_param:
+        try:
+            tz = pytz.timezone(tz_param)
+            # Example format: 2025-05-12 19:30:00 PDT-0700
+            current_time_str = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+            print(current_time_str)
+            message = f"Hello World! It is {current_time_str} in timezone {tz_param}."
+        except pytz.exceptions.UnknownTimeZoneError:
+            return jsonify({"error": "Invalid timezone designator: {tz_param}"}), 400
+
+    accept_header = request.headers.get('Accept')
+    if accept_header == 'application/json':
+        return jsonify({"message": message})
+    return message
+
+@app.route('/unravel', methods=['POST'])
+def unravel():
+    """
+    Takes a JSON body in the request and returns a flat list
+    of its keys and values in the order they appear.
+    """
+    data = request.get_json()
+    result = []
+    def unravel_helper(obj):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                result.append(k)
+                unravel_helper(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                unravel_helper(item)
+        else:
+            result.append(obj)
+    unravel_helper(data)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
